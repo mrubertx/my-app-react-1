1af7734c74134559bd8a16930b0c4f1a7a1e1bb9
Mariano Ruberto <marianorubertox@gmail.com>
2022-02-09 23:10:21 -0300

    añadi el itemcount mas los const de inicial y maximo

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,16 @@
+import ItemCount from "./components/ItemCount/ItemCount";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import NavBar from "./components/NavBar/NavBar";
 
 
 function App() {
+  const inicial=1
+  const maximo=10
   return (
     <div>
       <NavBar/>
       <ItemListContainer greeting="Bienvenidos a Comic Store"/>
+      <ItemCount inicial={inicial} maximo={maximo}/>
     </div>
   )
     
