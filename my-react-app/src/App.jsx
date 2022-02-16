f52cd4181c609dcc5c111a79c03f2f54ca522d50
Mariano Ruberto <marianorubertox@gmail.com>
2022-02-16 16:21:27 -0300

    agregue app jsx

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <ItemListContainer/>
+      <ItemDetailContainer/>
+    </div>
+  );
+    
+  
+}
+
+export default App;
