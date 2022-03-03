2beda9fd44b5911b03da2e84682e792043fa1728
Mariano Ruberto <marianorubertox@gmail.com>
2022-03-03 20:19:43 -0300

    cambie item detail

--- my-react-app/src/components/ItemDetail/ItemDetail.jsx
+++ my-react-app/src/components/ItemDetail/ItemDetail.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import ItemCount from '../ItemCount/ItemCount';
+import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 
-
-export default function ItemDetail({prod}) {
-    const inicial = 1
-    const maximo = 10
-    return (
-        <div>
-            <div key={prod.id} className="col-md-4">
-                <div className="card w-100 mt-5">
-                    <div className="card-body">
-                        <img src={prod.foto} alt="" className="w-50"/>
-                    </div>
-                    <div className="card-body">
-                        <h3>{`${prod.name}`}</h3>
-                    </div>
-                    <h2>${prod.price}</h2>
-                    <ItemCount inicial={inicial} maximo={maximo}/>
-                    <div className="card-body">
-                        {prod.descripcion}
-                    </div>
-                    <div className="card-footer">
-                        <buttom className="btn btn-outline-primary brn-block">
-                            COMPRAR
-                        </buttom>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+export const ItemDetail= ({item}) => {
+  const inicial = 1
+  const maximo = 10 
+  return (
+    <div>
+        <div className="col-md-4">
+                  <div className="card w-100 mt-5">
+                      <div className="card-body">
+                          <img src={item.foto} alt="" className="w-50"/>
+                      </div>
+                      <div className="card-body">
+                          <h3>{`${item.name}`}</h3>
+                      </div>
+                      <h2>${item.price}</h2>
+                      <ItemCount inicial={inicial} maximo={maximo}/>
+                      <h5>{item.descripcion}</h5>
+                      <div className="card-footer">
+                          <button className="btn btn-outline-primary brn-block">
+                              COMPRAR
+                          </button>
+                      </div>
+                  </div>
+              </div>
+    </div>
+  )
 }
