2ff232995c57d0b672992cf38f3c16599eb19cd5
Mariano Ruberto <marianorubertox@gmail.com>
2022-03-09 14:12:18 -0300

    añadi el agregarcompra y el link para finalizar la compra

--- my-react-app/src/components/ItemDetail/ItemDetail.jsx
+++ my-react-app/src/components/ItemDetail/ItemDetail.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import ItemCount from '../ItemCount/ItemCount'
 
 export const ItemDetail= ({item}) => {
 
+  const [visible, setVisible] = useState(true)
+
+  const agregarCompra= () => {
+    console.log(visible);
+    setVisible(false);
+  }
 
   return (
     <div>
@@ -14,13 +21,12 @@ export const ItemDetail= ({item}) => {
                       <div className="card-body">
                           <h3>{`${item.name}`}</h3>
                       </div>
-                      <h2>${item.price}</h2>
-                      <h5>Cantidad:</h5><ItemCount inicial={1} maximo={item.stock}/><h5>(Stock disponible:{item.stock})</h5>
-                      <h7>Descripcion: {item.descripcion}</h7>
-                          <button className="btn btn-dark brn-block">
-                              COMPRAR
-                          </button>
-                  </div>
+                          <h2>${item.price}</h2>
+                      <h6>Descripcion: {item.descripcion}</h6>
+                      <h5>({item.stock} disponibles)</h5>
+                      {visible && <ItemCount stock={item.stock} inicial={1} agregarCompra={agregarCompra}/>}
+                      <Link to={"/cart"} className='btn btn-primary brn-block'>Finalizar compra</Link>
+                      </div>
               </div>
     </div>
   )
